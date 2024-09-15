88bb6b00ea8fe9e19649fee135a645baa6cd9142
Lara-888 <uhvatova64@gmail.com>
2024-09-15 23:34:52 +0300

    1.3h

--- 1.3.c
+++ 1.3.c
@@ -1,11 +1,11 @@
 int main() {
     int number;
 
-    // Считываем число с клавиатуры
+ 
     printf("Enter your number: ");
     scanf("%d", &number);
 
-    // Выводим число, его квадрат и куб
+
     printf("%d -> %d -> %d\n", number, number * number, number * number * number);
 
     return 0;
